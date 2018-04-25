65ac83f924c4e476fc1fba534a5a91d0e11bfb80
Sophia Varella <sophiavarella@gmail.com>
2018-04-25 15:41:11 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 // Code Goes Here
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Order from './components/order'
+
+const iceCreamScoops = ['vanilla', 'mint'];
+
+ReactDom.render(
+  <Order
+    cone: true
+    scoops: {iceCreamScoops}
+    orderInfo: {{
+      customerName: "Sophia"
+      orderedAt: 24
+    }}
+  />,
+  document.getElementById('root')
+);
